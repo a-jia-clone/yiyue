0c0d0a040c5cbd41f712c0d14cfa0818faec04b7
Michael Jia <michael.jia@sage.com>
2016-10-05 19:54:55 -0700

    Added JQuery

--- CreateYue.aspx.cs
+++ CreateYue.aspx.cs
@@ -13,5 +13,29 @@ namespace YiYue
         {
 
         }
+
+        protected void btnCreate_Click(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            dsYue.InsertParameters.Clear();
+            dsYue.InsertParameters.Add(new Parameter("CreatedBy", System.Data.DbType.String, txtCreatedBy.Text));
+            dsYue.InsertParameters.Add(new Parameter("CreatedAt", System.Data.DbType.DateTime, now.ToString()));
+            dsYue.InsertParameters.Add(new Parameter("ModifiedAt", System.Data.DbType.DateTime, now.ToString()));
+            dsYue.InsertParameters.Add(new Parameter("Status", System.Data.DbType.Int16, "1"));
+            dsYue.InsertParameters.Add(new Parameter("YueName", System.Data.DbType.String, txtName.Text));
+            dsYue.InsertParameters.Add(new Parameter("YueDateTime", System.Data.DbType.DateTime, now.AddDays(1).ToString()));
+            dsYue.InsertParameters.Add(new Parameter("Duration", System.Data.DbType.String, txtDuration.Text));
+            dsYue.InsertParameters.Add(new Parameter("Minimum", System.Data.DbType.Int32, txtMinimum.Text));
+            dsYue.InsertParameters.Add(new Parameter("Maximum", System.Data.DbType.Int32, txtMaximum.Text));
+            dsYue.InsertParameters.Add(new Parameter("Tags", System.Data.DbType.String, ""));
+            dsYue.InsertParameters.Add(new Parameter("Description", System.Data.DbType.String, txtDescription.Text));
+            dsYue.InsertParameters.Add(new Parameter("Location", System.Data.DbType.String, txtLocation.Text));
+            dsYue.InsertParameters.Add(new Parameter("MapUrl", System.Data.DbType.String, ""));
+            dsYue.InsertParameters.Add(new Parameter("RegiterDue", System.Data.DbType.DateTime, now.AddDays(1).ToString()));
+            dsYue.InsertParameters.Add(new Parameter("Notes", System.Data.DbType.String, ""));
+            dsYue.Insert();
+
+            lblMessage.Text = "Inserted";
+        }
     }
 }
---
+++ obj/Release/Package/PackageTmp/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="YiYue.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Group Master</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <h1>易约再约</h1>
+        <p>感谢关注"易约再约"。
+易约再约是一个组织团队活动的得力工具，运动娱乐饮食等各类聚会活动都可以通过她来发起及报名。
+目前相关功能正在开发中，敬请期待。</p>
+    </div>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:YueAzureConnectionString %>" SelectCommand="SELECT * FROM [t_AccessControl]"></asp:SqlDataSource>
+    </form>
+</body>
+</html>
